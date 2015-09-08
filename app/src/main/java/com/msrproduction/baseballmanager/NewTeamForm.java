e59e90c26418f1be31ef1e0e401778ce07a06cb3
Steven Rowe <mrowe122@gmail.com>
2015-09-07 21:50:49 -0400

    Initial commit

--- app/src/main/java/com/msrproduction/baseballmanager/NewTeamForm.java
+++ app/src/main/java/com/msrproduction/baseballmanager/NewTeamForm.java
@@ -2,6 +2,7 @@ package com.msrproduction.baseballmanager;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class NewTeamForm extends AppCompatActivity{
 
@@ -9,5 +10,33 @@ public class NewTeamForm extends AppCompatActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_team_form);
+		initSetup();
+	}
+
+	private void initSetup() {
+		//add team button
+		(findViewById(R.id.add_new_team)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//addTeam();
+			}
+		});
+
+		//cancel button
+		findViewById(R.id.cancel_team).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+				overridePendingTransition(R.anim.abc_slide_in_bottom, R.anim.abc_slide_out_bottom);
+			}
+		});
+
+		//add field button
+		findViewById(R.id.add_new_field).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//addField();
+			}
+		});
 	}
 }
