948a6b33a3c85804492e0703e9e26bf75f96f463
Steven Rowe <mrowe122@gmail.com>
2015-09-15 22:36:12 -0400

    Initial commit

--- app/src/main/java/com/msrproduction/baseballmanager/MainActivity.java
+++ app/src/main/java/com/msrproduction/baseballmanager/MainActivity.java
@@ -3,14 +3,23 @@ package com.msrproduction.baseballmanager;
 import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.common.AccountPicker;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
@@ -21,6 +30,43 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initSetup();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == 1 && resultCode == RESULT_OK) {
+			String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+			userEmail = accountName;
+			((TextView) findViewById(R.id.email)).setText(accountName);
+		}
+	}
+
+	private void initSetup() {
+		ImageView iv = (ImageView) findViewById(R.id.baseball_icon);
+		switch (new Random().nextInt(6)) {
+			case 0:
+				iv.setImageResource(R.drawable.icon_a);
+				break;
+			case 1:
+				iv.setImageResource(R.drawable.icon_b);
+				break;
+			case 2:
+				iv.setImageResource(R.drawable.icon_c);
+				break;
+			case 3:
+				iv.setImageResource(R.drawable.icon_d);
+				break;
+			case 4:
+				iv.setImageResource(R.drawable.icon_e);
+				break;
+			case 5:
+				iv.setImageResource(R.drawable.icon_f);
+				break;
+			default:
+				iv.setImageResource(R.drawable.icon_e);
+				break;
+		}
 
 		findViewById(R.id.main_games).setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -56,15 +102,6 @@ public class MainActivity extends Activity {
 				getUsersEmail();
 			}
 		});
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == 1 && resultCode == RESULT_OK) {
-			String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-			userEmail = accountName;
-			((TextView) findViewById(R.id.email)).setText(accountName);
-		}
 	}
 
 	private void getUsersEmail() {
