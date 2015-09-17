cfb6751f4daafb287faefe142d130b7f06bbdaad
Steven Rowe <mrowe122@gmail.com>
2015-09-17 11:22:21 -0400

    Initial commit

--- app/src/main/java/com/msrproduction/baseballmanager/LeagueActivity.java
+++
@@ -1,6 +0,0 @@
-package com.msrproduction.baseballmanager;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class LeagueActivity extends AppCompatActivity{
-}
--- app/src/main/java/com/msrproduction/baseballmanager/MainActivity.java
+++ app/src/main/java/com/msrproduction/baseballmanager/MainActivity.java
@@ -3,12 +3,7 @@ package com.msrproduction.baseballmanager;
 import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,10 +13,9 @@ import android.widget.TextView;
 import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.common.AccountPicker;
 
-import java.lang.ref.WeakReference;
 import java.util.Random;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 	private final String LOG_TAG = MainActivity.class.getSimpleName();
 	public static String userEmail;
@@ -39,6 +33,24 @@ public class MainActivity extends Activity {
 			String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
 			userEmail = accountName;
 			((TextView) findViewById(R.id.email)).setText(accountName);
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.main_games:
+				startActivity(new Intent(MainActivity.this, GamesActivity.class));
+				break;
+			case R.id.main_teams:
+				startActivity(new Intent(MainActivity.this, TeamsActivity.class));
+				break;
+			case R.id.main_players:
+				startActivity(new Intent(MainActivity.this, PlayersActivity.class));
+				break;
+			case R.id.email:
+				getUsersEmail();
+				break;
 		}
 	}
 
@@ -68,40 +80,10 @@ public class MainActivity extends Activity {
 				break;
 		}
 
-		findViewById(R.id.main_games).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(MainActivity.this, GamesActivity.class));
-			}
-		});
-
-		findViewById(R.id.main_leagues).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(MainActivity.this, LeagueActivity.class));
-			}
-		});
-
-		findViewById(R.id.main_teams).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(MainActivity.this, TeamsActivity.class));
-			}
-		});
-
-		findViewById(R.id.main_players).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				startActivity(new Intent(MainActivity.this, PlayersActivity.class));
-			}
-		});
-
-		findViewById(R.id.email).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				getUsersEmail();
-			}
-		});
+		findViewById(R.id.main_games).setOnClickListener(this);
+		findViewById(R.id.main_teams).setOnClickListener(this);
+		findViewById(R.id.main_players).setOnClickListener(this);
+		findViewById(R.id.email).setOnClickListener(this);
 	}
 
 	private void getUsersEmail() {
